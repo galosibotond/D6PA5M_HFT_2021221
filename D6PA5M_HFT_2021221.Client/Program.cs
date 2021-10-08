9a9312566570aad4949a351f6897b6456e948415
Botond Galosi <galosi.botond@gmail.com>
2021-10-08 11:46:52 +0200

    mdf and ldf removed from gitignore

---
+++ D6PA5M_HFT_2021221.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace D6PA5M_HFT_2021221.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
