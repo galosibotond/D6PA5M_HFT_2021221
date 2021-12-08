db32a599defadddab1136851a562dc85fc8ce1b9
Botond Galosi <galosi.botond@gmail.com>
2021-12-08 19:07:22 +0100

    Removed unused usings in ClientHelper

--- D6PA5M_HFT_2021221.Client/Helpers/ClientHelper.cs
+++ D6PA5M_HFT_2021221.Client/Helpers/ClientHelper.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Text.Json;
-using System.Threading;
-using ConsoleTools;
-using D6PA5M_HFT_2021221.Models;
+﻿using ConsoleTools;
 
 namespace D6PA5M_HFT_2021221.Client
 {
