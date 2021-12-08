e9ad0c32725cc5a4d42371fb39b27e64faf608cf
Botond Galosi <galosi.botond@gmail.com>
2021-12-08 19:08:42 +0100

    Removed unused usings in StatAPIHelper

--- D6PA5M_HFT_2021221.Client/Helpers/StatAPIHelper.cs
+++ D6PA5M_HFT_2021221.Client/Helpers/StatAPIHelper.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using ConsoleTools;
 
 namespace D6PA5M_HFT_2021221.Client
