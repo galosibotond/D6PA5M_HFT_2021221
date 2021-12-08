9c4798b9e66bafa7761c6bfa6e2bed556ccf28b7
Botond Galosi <galosi.botond@gmail.com>
2021-12-08 19:16:45 +0100

    Added null check for rest service in the constructor of UpdateAPIHelper

--- D6PA5M_HFT_2021221.Client/Helpers/UpdateAPIHelper.cs
+++ D6PA5M_HFT_2021221.Client/Helpers/UpdateAPIHelper.cs
@@ -10,7 +10,7 @@ namespace D6PA5M_HFT_2021221.Client
 
         public UpdateAPIHelper(ConsoleMenu consoleMenu, RestService restService) : base(consoleMenu)
         {
-            this.restService = restService;
+            this.restService = restService ?? throw new ArgumentNullException(nameof(restService));
         }
 
         public void UpdateRecordCompany()
